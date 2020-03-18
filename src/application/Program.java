8863704b2069a6980fabdfc91f263c2a444abff0
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2020-03-18 17:07:02 -0300

    aplicando insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,7 +33,13 @@ public class Program {
 		for (Seller obj1 : list) {
 			System.out.println(obj1);
 		}
-
+		
+		/*"\n=== TEST 3: seller Insert =====");
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new Id "+newSeller.getId());
+		*/
+		
 	}
 
 }
