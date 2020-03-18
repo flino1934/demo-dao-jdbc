4e8b464b027b9ec9993cb79ca65fa6159b0ae5f8
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2020-03-18 11:18:42 -0300

    Implementacao do findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,10 +9,13 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Department obj = new Department(1, "Livros");
-		Seller seller = new Seller(21, "bob","bob@gmail.com",new Date(), 3000.0,obj);
-		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+
+
+		
 		
 		System.out.println(seller);
 		
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,13 +1,28 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 //Classe responsavel por implementar os DAO
 
 public class SellerDaoJDBC implements SellerDao{
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+	
+		this.conn = conn;
+		
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -29,8 +44,54 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"WHERE seller.Id = ?");
+			
+			st.setInt(1, id);//a operação esta passando como parametro o id que vai ser recebido como argumento pelo metodo
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				
+				//Foi criado um Department para passar os dados 
+				Department department = new Department();
+				department.setId(rs.getInt("DepartmentId"));//vai passar o ID que esta no banco para a classe!
+				department.setName(rs.getString("DepName"));//vai passar o nome do departamento para a classe!
+				
+				//Foi criado um Seller para passar os dados para a classe!
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(department);
+				
+				return obj;
+				
+			}
+			
+			return null;
+			
+		}catch(SQLException e) {
+			
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+		}
+		
 	}
 
 	@Override
