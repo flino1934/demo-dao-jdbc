68e6e9ad9deb671aae4b13af2f469a00c164614f
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2020-03-18 21:37:18 -0300

    implementacao update

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -83,6 +83,33 @@ public class SellerDaoJDBC implements SellerDao{
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
 		
+		PreparedStatement st = null;
+		try {
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+"WHERE Id = ?");
+			
+			st.setString(1, obj.getName());// o dado inseridovai ser o obj passado como argumento
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+
+			throw new DbException(e.getMessage());
+
+		} finally {
+
+			DB.closeStatement(st);
+
+		}
+
 	}
 
 	@Override
